4f240eb7b82cc38aef1ab4cbe052369663cca0f2
MenSeb <sebastien.menard.github@gmail.com>
2021-01-16 14:21:19 -0500

    update demo with basic example

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { } from '../src';
+import {
+    Accordion,
+    AccordionButton,
+    AccordionHeader,
+    AccordionPanel,
+    AccordionProvider,
+    AccordionSection,
+} from '../src';
 import './styles.scss';
 
 function Demo() {
-    return null;
+    return (
+        <AccordionProvider
+            alwaysExpand={true}
+            multipleExpand={true}
+            numberOfAccordions={5}
+        >
+            <Accordion>
+                {Array.from({ length: 5 }, (_, index) => (
+                    <AccordionSection key={index}>
+                        <AccordionHeader>
+                            <AccordionButton
+                                controls={`panel-${index}`}
+                                id={`button-${index}`}
+                            >
+                                {`button-${index}`}
+                            </AccordionButton>
+                        </AccordionHeader>
+                        <AccordionPanel
+                            labelledby={`button-${index}`}
+                            id={`panel-${index}`}
+                        >
+                            {`panel-${index}`}
+                        </AccordionPanel>
+                    </AccordionSection>
+                ))}
+            </Accordion>
+        </AccordionProvider>
+    );
 }
 
 render(<Demo />, document.getElementById('root'));
